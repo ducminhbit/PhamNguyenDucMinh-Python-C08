9652b6bf7a7153ada8af61d1e861744b5690ea31
ducminhbit <vitupham247@gmail.com>
2019-09-14 21:32:10 +0700

    '.'

---
+++ python/input_number.py
@@ -0,0 +1,8 @@
+while True:
+    txt = input("nhap mat khau ")
+    elif txt.isalpha() < 8:
+        print("mat khau sai")
+    else:
+        print("mat khau dung")
+        break
+   
---
+++ python/turtle_ex.py
@@ -0,0 +1,25 @@
+from turtle import *
+shape("turtle")
+color("red")
+speed(-0.00000001)
+
+
+for i in range(1, 100000000, 100):
+    # forward(100)
+    # left(90)
+
+ forward(100)
+ left(90)
+ forward(100)
+ left(90)
+ forward(100)
+ left(90)
+ forward(100)
+ left(90)
+
+
+ right(-2)
+
+
+
+mainloop()
